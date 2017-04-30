5e6791df8ac28573a51f48bfa0b054e112b19791
Carter Parks <carter@carterparks.com>
2017-04-30 16:38:08 -0600

    changed default timezone to mtn time

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,8 @@ module Marketplace
   class Application < Rails::Application
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded.
+    # -- all .rb
+    config.time_zone = 'Mountain Time (US & Canada)'
+    config.active_record.default_timezone = :local # Or :utc files in that directory are automatically loaded.
   end
 end
