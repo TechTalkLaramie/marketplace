f0749ad318a32ca070b239954fe43ba732beb858
lkubie <lenore.kubi@gmail.com>
2017-04-29 19:49:39 -0600

    prettified create new service

---
+++ app/assets/javascripts/perHourToggle.js
@@ -0,0 +1,26 @@
+$(document).on('turbolinks:load', function() {
+	
+	$("#container-newServiceToggle").on("click", function(){
+		var widthOfSwitch = $(this).width()
+		var distanceToMove = $(this).width()/2+"px"
+		var currentOffset = $("#module-toggleCover").css("right")
+		//console.log(currentOffset)
+		if(currentOffset == "0px"){
+			//$("#module-toggleCover").css("right", distanceToMove)
+			$("#module-toggleCover").animate({"right": distanceToMove})
+			$("#priceInputPerText").text("flat-rate")
+			$("#fixedRateCheckboxNewService").attr('checked', true)
+		}
+		else{
+			//$("#module-toggleCover").css("right", "0px")
+			$("#module-toggleCover").animate({"right": "0px"})
+			$("#priceInputPerText").text("per hour")
+			$("#fixedRateCheckboxNewService").attr('checked', false)
+		}
+		
+	})
+	
+	
+	
+	
+});
