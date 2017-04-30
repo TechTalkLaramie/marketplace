c45427d886734bbd2353e68906eb79120af3fbdb
Carter Parks <carter@carterparks.com>
2017-04-29 18:21:38 -0600

    offers can be accepted

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,12 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get "/dashboard", to: "dashboard#index"
   resources :services do
-    resources :orders
+    resources :orders do
+      member do
+        post "accept"
+        post "cancel"
+      end
+    end
   end
   resources :profiles, only: [:show] do
 	  collection do
